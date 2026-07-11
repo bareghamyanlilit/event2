c880dfe69b7e8ca6a498c4470c45cf093b6ca370
Lilit <baregamyanlilit36@gmail.com>
2026-07-11 15:30:23 +0400

    .

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,19 +3,15 @@ import Link from "next/link";
 
 export function Footer() {
   return (
-    <footer className="bottom-0 py-10 text-center text-sm bg-[#1b1b1b]   text-[#fcfbfb]">
-      <p>
-        <a href={phones[2]}>{phones[0]} | {phones[1]}</a>
-      </p>
-      <p>
-        <a href={phones[5]}>{phones[3]} | {phones[4]}</a>
-      </p>
-      <hr className="my-5" />
-      <Link href="https://www.instagram.com/siteup.am/">
-        <p className="mt-2">
-          Պատրաստվել է <span className="underline">siteup.am</span>-ի կողմից
+    <footer className="bottom-0 py-5 text-center text-sm bg-[#1b1b1b]   text-[#fcfbfb]">
+      
+      <Link href="https://siteup-am.vercel.app/">
+        <p className="mt-2 text-xl">
+          Պատրաստվել է <span className="underline">siteup.am</span> -ի  կողմից
         </p>
       </Link>
+      <hr className="my-5" />
+      <a href="tel:+37477760204" className="">SiteUp : +374 77 760 204</a>
     </footer>
   );
 }
